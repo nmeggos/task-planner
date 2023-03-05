345e5c143a05f0442f167a0a5c43b831e55696d1
Nikitas Meggos <meggosni@live.com>
2023-03-05 16:14:39 -0600

    adding initial todo model with create todo test

---
+++ src/TaskPlanner.Core/Domain/Tasks/Todo.cs
@@ -0,0 +1,26 @@
+﻿namespace TaskPlanner.Core.Domain.Tasks;
+
+public class Todo
+{
+    private Todo()
+    {
+        
+    }
+    
+    private Todo(string title, string description)
+    {
+        Title = title;
+        Description = description;
+    }
+
+    public string Title { get; private set; }
+
+    public string Description { get; private set; }
+
+    public DateTime? DueOne { get; private set; }
+
+    public static Todo Create(string title, string description)
+    {
+        return new Todo(title, description);
+    }
+}
---
+++ tests/TaskPlanner.UnitTests/CreateTodoTests.cs
@@ -0,0 +1,26 @@
+﻿using FluentAssertions;
+
+namespace TaskPlanner.UnitTests;
+
+public class CreateTaskTests
+{
+    [Fact]
+    public void WhenCreatingTodo_ShouldHaveRequiredFields()
+    {
+        var sut = Todo.Create("SomeTitle", "SomeDescription");
+
+        sut.Should().NotBeNull();
+        
+        sut.Title
+            .Should()
+            .NotBeNull()
+            .And
+            .NotBeNull();
+        
+        sut.Description
+            .Should()
+            .NotBeNull()
+            .And
+            .NotBeNull();
+    }
+}
